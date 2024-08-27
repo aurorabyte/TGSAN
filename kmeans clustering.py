83076dd768d7b1db55f06e3aee803d4005c0f12a
aurorabyte <153363134+aurorabyte@users.noreply.github.com>
2024-08-27 20:33:09 +0800

    Update kmeans clustering.py

--- kmeans clustering.py
+++ kmeans clustering.py
@@ -1,5 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
 
+
+X = pd.read_csv('./.../feature.csv')['feature_name'].dropna().values.reshape(-1, 1)
 
 k_values = range(min_num, max_num)
 inertia, centroids, rate = [], [], []
@@ -11,4 +17,13 @@ for k in k_values:
     centroids.append(kmeans.cluster_centers_)
 for i in range(0, len(inertia)-1):
     rate[i+1] = (inertia[i]-inertia[i+1])/inertia[i]
+    print(rate)
 
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.plot(k_values, inertia, color='black')
+plt.plot(k_values, inertia, marker='s', color='none', markeredgecolor='red', markeredgewidth=2)
+plt.xlabel('Number of Clusters (${L}$)', font={'family': 'TImes New Roman', 'size': 16})
+plt.ylabel('Sum of Squared Errors (SSE)', font={'family': 'TImes New Roman', 'size': 16})
+plt.xticks(fontproperties='TImes New Roman', size=16)
+plt.yticks(fontproperties='TImes New Roman', size=16)
+plt.show()
